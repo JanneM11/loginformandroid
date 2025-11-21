cac4eaa494d9240e349f93362d69046c5972b509
JanneM11 <jannem92@outlook.com>
2025-11-21 14:08:39 +0200

    Tehty

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.loginform.ui.theme.LoginFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +39,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoginFormTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MyApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +49,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MyApp(modifier: Modifier = Modifier) {
+    var password: String by remember { mutableStateOf("") }
+    var username: String by remember { mutableStateOf("") }
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp, vertical = 8.dp)
+    Column(
         modifier = modifier
-    )
-}
+    ) {
+        Text(
+            text = "Login",
+            textAlign = TextAlign.Center,
+            color = Color.Blue,
+            modifier = appModifier,
+            style = MaterialTheme.typography.titleLarge,
+        )
+        OutlinedTextField(
+            value= username,
+            onValueChange = {username = it},
+            label = {
+                Text(
+                    text = "Username"
+                )
+            },
+            modifier = appModifier,
+            singleLine = true,
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Email icon",
+                    tint = Color.Black
+                )
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
+        )
+        OutlinedTextField(
+            value= password,
+            onValueChange = {password = it},
+            label = {
+                Text(
+                    text = "Password"
+                )
+            },
+            modifier = appModifier,
+            singleLine = true,
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "Lock icon",
+                    tint = Color.Black
+                )
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(
+            onClick = {},
+            modifier = appModifier
+        ) {
+            Text("Submit")
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoginFormTheme {
-        Greeting("Android")
     }
 }
